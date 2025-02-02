2e0616d384fe1953cdb3604fc8c3a3dbf30ae180
Ashish Nevan <nevchris242@gmail.com>
2025-02-01 23:57:00 -0500

    added background cleanup of temp files aws now uses env variables

--- backend/api.py
+++ backend/api.py
@@ -1,32 +1,39 @@
 import os
 from pathlib import Path
-from pydantic import BaseModel
-from fastapi import FastAPI, UploadFile, HTTPException, status
-from fastapi.responses import FileResponse
-from pipelines import standardize_docling, standardize_markitdown, html_to_md_docling, get_job_name, pdf_to_md_docling, clean_temp_files
 
+from dotenv import load_dotenv
+from fastapi import FastAPI, UploadFile, HTTPException, status, BackgroundTasks
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+from pipelines import standardize_docling, standardize_markitdown, html_to_md_docling, get_job_name, \
+    pdf_to_md_docling, clean_temp_files
+
+load_dotenv('../.env')
 app = FastAPI()
+
 
 class URLRequest(BaseModel):
     url: str
 
+
 @app.post("/processurl/", status_code=status.HTTP_200_OK)
-async def process_url(request: URLRequest):
-    clean_temp_files()
+async def process_url(request: URLRequest, background_tasks: BackgroundTasks):
     try:
         url = request.url
         job_name = get_job_name()
         markdown_output = html_to_md_docling(url, job_name)
+        background_tasks.add_task(my_background_task)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-    return FileResponse(markdown_output, media_type='application/octet-stream',filename=f'{job_name}.md')
+    return FileResponse(markdown_output, media_type='application/octet-stream', filename=f'{job_name}.md')
 
 
 @app.post("/processpdf/", status_code=status.HTTP_200_OK)
-async def process_pdf(file: UploadFile):
+async def process_pdf(file: UploadFile, background_tasks: BackgroundTasks):
     if file.content_type != 'application/pdf':
         raise HTTPException(status_code=400, detail="File must be a PDF")
-    clean_temp_files()
+    background_tasks.add_task(my_background_task)
     contents = await file.read()
     output = Path("./temp_processing/output/pdf")
     os.makedirs(output, exist_ok=True)
@@ -45,10 +52,10 @@ async def process_pdf(file: UploadFile):
 
 
 @app.post('/standardizedocling/', status_code=status.HTTP_200_OK)
-async def standardizedocling(file: UploadFile):
+async def standardizedocling(file: UploadFile, background_tasks: BackgroundTasks):
     if file.content_type != 'application/pdf':
         raise HTTPException(status_code=400, detail="File must be a PDF")
-    clean_temp_files()
+    background_tasks.add_task(my_background_task)
     contents = await file.read()
     output = Path("./temp_processing/output/pdf")
     os.makedirs(output, exist_ok=True)
@@ -65,13 +72,14 @@ async def standardizedocling(file: UploadFile):
         await file.close()
     return FileResponse(standardized_output, media_type='application/octet-stream', filename=f'{file.filename}.md')
 
+
 @app.post('/standardizemarkitdown/', status_code=status.HTTP_200_OK)
-async def standardizemarkitdown(file: UploadFile):
+async def standardizemarkitdown(file: UploadFile, background_tasks: BackgroundTasks):
     if file.content_type != 'application/pdf':
         raise HTTPException(status_code=400, detail="File must be a PDF")
-    clean_temp_files()
+    background_tasks.add_task(my_background_task)
     contents = await file.read()
-    output = Path("./output/pdf")
+    output = Path("./temp_processing/output/pdf")
     os.makedirs(output, exist_ok=True)
     job_name = get_job_name()
     try:
@@ -85,3 +93,8 @@ async def standardizemarkitdown(file: UploadFile):
     finally:
         await file.close()
     return FileResponse(standardized_output, media_type='application/octet-stream', filename=f'{file.filename}.md')
+
+
+def my_background_task():
+    clean_temp_files()
+    print("Performed cleanup of temp files.")
