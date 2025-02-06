a12fa235cfb3a5ce7dd403b0ec05e979afc59466
Ashish Nevan <nevchris242@gmail.com>
2025-02-06 00:36:07 -0500

    FIX: api fail when some of the query are not satisfied

--- backend/api.py
+++ backend/api.py
@@ -1,77 +1,69 @@
 import os
+import zipfile
+from io import BytesIO
 from pathlib import Path
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, UploadFile, HTTPException, status, BackgroundTasks, Query
 from fastapi.responses import FileResponse, StreamingResponse
 from pydantic import BaseModel
-from io import BytesIO
-import zipfile
 
-from pipelines import standardize_docling, standardize_markitdown, html_to_md_docling, get_job_name, \
-    pdf_to_md_docling, clean_temp_files
+from pipelines import (
+    standardize_docling,
+    standardize_markitdown,
+    html_to_md_docling,
+    get_job_name,
+    pdf_to_md_docling,
+    clean_temp_files,
+)
 
-load_dotenv('../.env')
+load_dotenv("../.env")
 app = FastAPI()
+
 
 class URLRequest(BaseModel):
     url: str
 
+
 @app.post("/processurl/", status_code=status.HTTP_200_OK)
 async def process_url(
-    background_tasks: BackgroundTasks,
-    request: URLRequest,
-    include_markdown: bool = Query(False),
-    include_images: bool = Query(False),
-    include_tables: bool = Query(False),
-    bundle: bool = Query(False)
+        background_tasks: BackgroundTasks,
+        request: URLRequest,
+        include_markdown: bool = Query(False),
+        include_images: bool = Query(False),
+        include_tables: bool = Query(False),
 ):
+    if not any([include_markdown, include_images, include_tables]):
+        raise HTTPException(
+            status_code=400, detail="At least one output type must be selected"
+        )
     try:
         url = request.url
         job_name = get_job_name()
         result = html_to_md_docling(url, job_name)
         background_tasks.add_task(my_background_task)
 
-        if bundle:
-            # Create a ZIP archive
-            zip_buffer = BytesIO()
-            with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-                # Markdown
-                if include_markdown:
-                    if not os.path.exists(result['markdown']):
-                        raise HTTPException(status_code=500, detail="Markdown couldn't be generated. Maybe webpage has no data.")
-
-                    zip_file.write(result['markdown'], arcname="document.md")
-
-                # Images
-                if include_images:
-                    if not any(os.scandir(result['images'])):
-                        raise HTTPException(status_code=500, detail="No images found in the input webpage.")
-
-                    for img in result['images'].iterdir():
-                        zip_file.write(img, arcname=f"images/{img.name}")
-
-                # Tables
-                if include_tables:
-                    if not any(os.scandir(result['tables'])):
-                        raise HTTPException(status_code=500, detail="No tables found in the input webpage.")
-                        
-                    for table in result['tables'].iterdir():
-                        zip_file.write(table, arcname=f"tables/{table.name}")
-
-            zip_buffer.seek(0)
+        if include_images or include_tables:  # images or tables are requested
+            flag, zip_buffer, messages = create_zip_archive(result, include_markdown, include_images, include_tables)
             return StreamingResponse(
-                zip_buffer,
-                media_type='application/zip',
-                headers={"Content-Disposition" : f"attachment; filename={job_name}.zip"}
-            )
+                    zip_buffer,
+                    media_type="application/zip",
+                    headers={
+                        "Content-Disposition": f"attachment; filename={job_name}.zip"
+                    }) if flag else HTTPException(status_code=500, detail=messages)
         else:
-            if not include_markdown and not bundle:
-                raise HTTPException(status_code=400, detail="Only markdown output is supported for a non-bundled output")
+            if not result["markdown"]:
+                raise HTTPException(
+                    status_code=500,
+                    detail="Markdown couldn't be generated. Maybe webpage has no data.",
+                )
             return FileResponse(
-                result['markdown'],
+                result["markdown"],
                 media_type="application/octet-stream",
-                filename=f"{job_name}.md"
+                headers={
+                    "Content-Disposition": f"attachment; filename={job_name}.md"
+                },
+                filename=f"{job_name}.md",
             )
 
     except Exception as e:
@@ -80,81 +72,69 @@ async def process_url(
 
 @app.post("/processpdf/", status_code=status.HTTP_200_OK)
 async def process_pdf(
-    background_tasks: BackgroundTasks,
-    file: UploadFile, 
-    include_markdown: bool = Query(False),
-    include_images: bool = Query(False),
-    include_tables: bool = Query(False),
-    bundle: bool = Query(False)
+        background_tasks: BackgroundTasks,
+        file: UploadFile,
+        include_markdown: bool = Query(False),
+        include_images: bool = Query(False),
+        include_tables: bool = Query(False),
 ):
-    if file.content_type != 'application/pdf':
+    if not any([include_markdown, include_images, include_tables]):
+        raise HTTPException(
+            status_code=400, detail="At least one output type must be selected"
+        )
+
+    if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="File must be a PDF")
 
-    background_tasks.add_task(my_background_task)
-    contents = await file.read()
-    output = Path("./temp_processing/output/pdf")
-    os.makedirs(output, exist_ok=True)
-    job_name = get_job_name()
-
     try:
-        file_path = output / f'{job_name}.pdf'
-        with open(file_path, 'wb') as f:
+        background_tasks.add_task(my_background_task)
+        contents = await file.read()
+        output = Path("./temp_processing/output/pdf")
+        os.makedirs(output, exist_ok=True)
+        job_name = get_job_name()
+
+        file_path = output / f"{job_name}.pdf"
+        with open(file_path, "wb") as f:
             f.write(contents)
             await file.close()
 
         result = pdf_to_md_docling(file_path, job_name)
 
-        if bundle:
-            # Create a ZIP archive
-            zip_buffer = BytesIO()
-            with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-                # Markdown
-                if include_markdown:
-                    if not os.path.exists(result['markdown']):
-                        raise HTTPException(status_code=500, detail="Markdown couldn't be generated. Maybe PDF has no data.")
-
-                    zip_file.write(result['markdown'], arcname="document.md")
-
-                # Images
-                if include_images:
-                    if not any(os.scandir(result['images'])):
-                        raise HTTPException(status_code=500, detail="No images found in the input PDF.")
-
-                    for img in result['images'].iterdir():
-                        zip_file.write(img, arcname=f"images/{img.name}")
-
-                # Tables
-                if include_tables:
-                    if not any(os.scandir(result['tables'])):
-                        raise HTTPException(status_code=500, detail="No tables found in the input PDF.")
-
-                    for table in result['tables'].iterdir():
-                        zip_file.write(table, arcname=f"tables/{table.name}")
-
-            zip_buffer.seek(0)
-            return StreamingResponse(
-                zip_buffer,
-                media_type='application/zip',
-                headers={"Content-Disposition" : f"attachment; filename={job_name}.zip"}
+        if include_images or include_tables:  # images or tables are requested
+            flag, zip_buffer, messages = create_zip_archive(result, include_markdown, include_images,
+                                                            include_tables)
+            return (
+                StreamingResponse(
+                    zip_buffer,
+                    media_type="application/zip",
+                    headers={
+                        "Content-Disposition": f"attachment; filename={job_name}.zip"
+                    },
+                )
+                if flag
+                else HTTPException(status_code=500, detail=messages)
             )
         else:
-            if not include_markdown and not bundle:
-                raise HTTPException(status_code=400, detail="Only markdown output is supported for a non-bundled output")
-
+            if not result["markdown"] or not os.path.exists(result["markdown"]):
+                raise HTTPException(
+                    status_code=500,
+                    detail="Markdown couldn't be generated. Maybe webpage has no data.",
+                )
             return FileResponse(
-                result['markdown'],
+                result["markdown"],
                 media_type="application/octet-stream",
-                filename=f"{job_name}.md"
+                filename=f"{job_name}.md",
             )
-                
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     finally:
         await file.close()
 
-@app.post('/standardizedocling/', status_code=status.HTTP_200_OK)
+
+@app.post("/standardizedocling/", status_code=status.HTTP_200_OK)
 async def standardizedocling(file: UploadFile, background_tasks: BackgroundTasks):
-    if file.content_type != 'application/pdf':
+    if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="File must be a PDF")
     background_tasks.add_task(my_background_task)
     contents = await file.read()
@@ -162,8 +142,8 @@ async def standardizedocling(file: UploadFile, background_tasks: BackgroundTasks
     os.makedirs(output, exist_ok=True)
     job_name = get_job_name()
     try:
-        file_path = output / f'{job_name}.pdf'
-        with open(file_path, 'wb') as f:
+        file_path = output / f"{job_name}.pdf"
+        with open(file_path, "wb") as f:
             f.write(contents)
             await file.close()
         standardized_output = standardize_docling(file_path, job_name)
@@ -171,12 +151,16 @@ async def standardizedocling(file: UploadFile, background_tasks: BackgroundTasks
         raise HTTPException(status_code=500, detail=str(e))
     finally:
         await file.close()
-    return FileResponse(standardized_output, media_type='application/octet-stream', filename=f'{file.filename}.md')
+    return FileResponse(
+        standardized_output,
+        media_type="application/octet-stream",
+        filename=f"{file.filename}.md",
+    )
 
 
-@app.post('/standardizemarkitdown/', status_code=status.HTTP_200_OK)
+@app.post("/standardizemarkitdown/", status_code=status.HTTP_200_OK)
 async def standardizemarkitdown(file: UploadFile, background_tasks: BackgroundTasks):
-    if file.content_type != 'application/pdf':
+    if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="File must be a PDF")
     background_tasks.add_task(my_background_task)
     contents = await file.read()
@@ -184,8 +168,8 @@ async def standardizemarkitdown(file: UploadFile, background_tasks: BackgroundTa
     os.makedirs(output, exist_ok=True)
     job_name = get_job_name()
     try:
-        file_path = output / f'{job_name}.pdf'
-        with open(file_path, 'wb') as f:
+        file_path = output / f"{job_name}.pdf"
+        with open(file_path, "wb") as f:
             f.write(contents)
             await file.close()
         standardized_output = standardize_markitdown(file_path, job_name)
@@ -193,7 +177,50 @@ async def standardizemarkitdown(file: UploadFile, background_tasks: BackgroundTa
         raise HTTPException(status_code=500, detail=str(e))
     finally:
         await file.close()
-    return FileResponse(standardized_output, media_type='application/octet-stream', filename=f'{file.filename}.md')
+    return FileResponse(
+        standardized_output,
+        media_type="application/octet-stream",
+        filename=f"{file.filename}.md",
+    )
+
+
+def create_zip_archive(result, include_markdown, include_images, include_tables):
+    flag = False
+    messages = []
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        # Markdown
+        if include_markdown:
+            if not result["markdown"]:
+                messages.append(
+                    "Markdown couldn't be generated. Maybe webpage has blockers."
+                )
+            else:
+                zip_file.write(
+                    result["markdown"], arcname="document.md"
+                )
+                flag = flag or True
+
+        # Images
+        if include_images:
+            if not result["images"]:
+                messages.append("No images found in the input webpage.")
+            else:
+                for img in result["images"].iterdir():
+                    zip_file.write(img, arcname=f"images/{img.name}")
+                flag = flag or True
+
+        # Tables
+        if include_tables:
+            if not result["tables"]:
+                messages.append("No tables found in the input webpage.")
+            else:
+                for table in result["tables"].iterdir():
+                    zip_file.write(table, arcname=f"tables/{table.name}")
+                flag = flag or True
+
+    zip_buffer.seek(0)
+    return flag, zip_buffer, messages
 
 
 def my_background_task():
